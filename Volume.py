6e9b999b4977e284495ba83df523c6101695e123
Deepak364-max <deepaksrm343@gmail.com>
2026-01-11 12:25:16 +0530

    Update Volume.py

--- Volume.py
+++ Volume.py
@@ -1,1 +1,22 @@
+import requests
+
+# ONTAP cluster details
+CLUSTER = "https://	192.168.0.101"
+USER = "admin"
+PASS = "Netapp1!"
+
+# Volume details
+payload = {
+    "name": "vol_3",
+    "svm": {"name": "svm1"},
+    "aggregates": [{"name": "cluster1_01_SSD_1"}],
+    "size": 1073741824  # 1 GiB in bytes
+}
+
+# Send POST request
+url = f"{CLUSTER}/api/storage/volumes"
+response = requests.post(url, json=payload, auth=(USER, PASS), verify=False)
+
+print("Status:", response.status_code)
+print("Response:", response.json())
 
