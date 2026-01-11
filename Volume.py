32b9e51db2b332cc3bdaf468c21a1a0d1908bec0
Deepak364-max <deepaksrm343@gmail.com>
2026-01-11 14:52:47 +0530

    Update Volume.py

--- Volume.py
+++ Volume.py
@@ -3,12 +3,13 @@ import requests
 
 # ONTAP cluster details
 CLUSTER = "https://192.168.0.101"   # no tab or spaces
-USER = "admin"
-PASS = "Netapp1!"
+#USER = "admin"
+#PASS = "Netapp1!"
+header = {"Authorization": "Basic YWRtaW46TmV0YXBwMSE="}
 
 # Volume details
 payload = {
-    "name": "vol_3",
+    "name": "vol_4",
     "svm": {"name": "svm1"},
     "aggregates": [{"name": "cluster1_01_SSD_1"}],
     "size": 1073741824  # 1 GiB in bytes
@@ -18,7 +19,7 @@ url = f"{CLUSTER}/api/storage/volumes"
 
 # add timeout and better error handling
 try:
-    r = requests.post(url, json=payload, auth=(USER, PASS), verify=False, timeout=15)
+    r = requests.post(url, json=payload, headers=header, verify=False, timeout=15)
     print("Status:", r.status_code)
     print("Response:", r.json() if r.headers.get("Content-Type","").startswith("application/json") else r.text)
 except requests.RequestException as e:
